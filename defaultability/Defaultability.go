6f36ba2b93cd41b43cbc7d6ca1279312fd406199
Rui Ma <marui_06@outlook.com>
2022-12-09 23:21:27 +0800

    golang topsdk

--- defaultability/Defaultability.go
+++ defaultability/Defaultability.go
@@ -36,3 +36,22 @@ func (ability *Defaultability) TaobaoKfcKeywordSearch(req *request.TaobaoKfcKeyw
     }
     return &respStruct,err
 }
+/*
+    淘宝客-推广者-淘口令预警及拦截查询
+*/
+func (ability *Defaultability) TaobaoTbkDgTpwdRiskReport(req *request.TaobaoTbkDgTpwdRiskReportRequest) (*response.TaobaoTbkDgTpwdRiskReportResponse,error) {
+    if(ability.Client == nil) {
+        return nil,errors.New("Defaultability topClient is nil")
+    }
+    var jsonStr,err = ability.Client.Execute("taobao.tbk.dg.tpwd.risk.report",req.ToMap(),req.ToFileMap())
+    var respStruct = response.TaobaoTbkDgTpwdRiskReportResponse{}
+    if(err != nil){
+        log.Println("taobaoTbkDgTpwdRiskReport error",err)
+        return &respStruct,err
+    }
+    err = util.HandleJsonResponse(jsonStr, &respStruct)
+    if(respStruct.Body == "" || len(respStruct.Body) == 0) {
+        respStruct.Body = jsonStr
+    }
+    return &respStruct,err
+}
